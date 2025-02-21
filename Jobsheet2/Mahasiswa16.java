863bd825cbbc600942b7d8752cb2a91e9a622bd3
zakariahaniya <zakalvha@gmail.com>
2025-02-21 08:57:36 +0700

    jobsheet 2

---
+++ Jobsheet2/Mahasiswa16.java
@@ -0,0 +1,39 @@
+package Jobsheet2;
+
+public class Mahasiswa16 {
+    String nama;
+    String nim;
+    String kelas;
+    double ipk;
+
+    void tampilkanInformasi() {
+        System.out.println("Nama : " + nama);
+        System.out.println("NIM : " + nim);
+        System.out.println("IPK : " + ipk);
+        System.out.println("Kelas : " + kelas);
+    }
+
+    void ubahKelas(String kelasBaru) {
+        kelas = kelasBaru;
+    }
+    
+    void updateIpk(double ipkBaru) {
+        if ((ipk < 0.0) || (ipk > 4.0 )) {
+            System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0");
+        } else {
+            ipk = ipkBaru;
+        }
+    }
+
+    String nilaiKinerja() {
+        if (ipk >= 3.5) {
+            return "Kinerja sangat baik";
+        } else if (ipk >= 3.0) {
+            return "Kinerja baik";
+        } else if (ipk > 2.0) {
+            return "Kinerja cukup";
+        } else {
+            return "Kinerja kurang";
+        }
+    }
+}
