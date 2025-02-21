7fc3803e589e3034984d3ac52c6928b6a963d85e
zakariahaniya <zakalvha@gmail.com>
2025-02-21 09:10:33 +0700

    jobsheet 2

---
+++ Jobsheet2/MahasiswaMain16.java
@@ -0,0 +1,16 @@
+package Jobsheet2;
+
+public class MahasiswaMain16 {
+    public static void main(String[] args) {
+        Mahasiswa16 mhs1 = new Mahasiswa16();
+        mhs1.nama = "Muhammad Ali Farhan";
+        mhs1.nim = "2241720171";
+        mhs1.kelas = "SI 2J";
+        mhs1.ipk = 3.55;
+
+        mhs1.tampilkanInformasi();
+        mhs1.ubahKelas("SI 2K");
+        mhs1.updateIpk(3.60);
+        mhs1.tampilkanInformasi();
+    }
+}
