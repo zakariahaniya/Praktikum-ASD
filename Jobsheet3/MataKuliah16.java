1e6ea17b3ce78d95bf359a3a4c24fe4556038e1c
zakariahaniya <zakalvha@gmail.com>
2025-02-28 22:03:34 +0700

    jobsheet 3

--- Jobsheet3/MataKuliah16.java
+++ Jobsheet3/MataKuliah16.java
@@ -1,10 +1,12 @@
 package Jobsheet3;
+import java.util.Scanner;
 
 public class MataKuliah16 {
     public String kode;
     public String nama;
     public int sks;
     public int jumlahJam;
+    Scanner sc = new Scanner(System.in);
 
     public MataKuliah16(String kode, String nama, int sks, int jumlahJam) {
         this.kode = kode;
@@ -14,6 +16,30 @@ public class MataKuliah16 {
     }
 
     public MataKuliah16() {
-        
+
+    }
+
+    void tambahData(String kode, String nama,String dummy, int sks, int jumlahJam, int i) {
+        System.out.println("Masukkan Data Mata Kuliah Ke-" + (i + 1));
+        System.out.print("Kode       : ");
+        this.kode = sc.nextLine();
+        System.out.print("Nama       : ");
+        this.nama = sc.nextLine();
+        System.out.print("SKS        : ");
+        dummy = sc.nextLine();
+       this.sks = Integer.parseInt(dummy);
+        System.out.print("Jumlah Jam : ");
+        dummy = sc.nextLine();
+        this.jumlahJam = Integer.parseInt(dummy);
+        System.out.println("--------------------------------");
+    }
+
+    void cetakInfo(int i) {
+        System.out.println("Data Mata Kuliah ke-" + (i + 1));
+        System.out.println("Kode        : " + kode);
+        System.out.println("Nama        : " + nama);
+        System.out.println("SKS         : " + sks);
+        System.out.println("Jumlah Jam  : " + jumlahJam);
+        System.out.println("---------------------------------");
     }
 }
--- Jobsheet3/MataKuliahDemo16.java
+++ Jobsheet3/MataKuliahDemo16.java
@@ -3,27 +3,18 @@ import java.util.Scanner;
 public class MataKuliahDemo16 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        MataKuliah16[] arrayOfMataKuliah = new MataKuliah16[3];
-        String kode, nama, dummy;
-        int sks, jumlahJam;
+        System.out.print("Masukkan Jumlah Mata Kuliah : ");
+        int jumlahMk = sc.nextInt();
+        System.out.println("------------------------------------");
+        MataKuliah16[] arrayOfMataKuliah = new MataKuliah16[jumlahMk];
+        String kode = null, nama = null, dummy = null;
+        int sks = 0, jumlahJam = 0;
 
         for (int i = 0; i < 3; i++) {
-            System.out.println("Masukkan Data Mata Kuliah Ke-" + (i + 1));
-            System.out.print("Kode       : ");
-            kode = sc.nextLine();
-            System.out.print("Nama       : ");
-            nama = sc.nextLine();
-            System.out.print("SKS        : ");
-            dummy = sc.nextLine();
-            sks = Integer.parseInt(dummy);
-            System.out.print("Jumlah Jam : ");
-            dummy = sc.nextLine();
-            jumlahJam = Integer.parseInt(dummy);
-            System.out.println("--------------------------------");
-
             arrayOfMataKuliah[i] = new MataKuliah16(kode, nama, sks, jumlahJam);
+            arrayOfMataKuliah[i].tambahData(kode, nama, dummy, sks, jumlahJam, i);
         }
-
+        
         arrayOfMataKuliah[0] = new MataKuliah16();
 
         arrayOfMataKuliah[0].kode = "123";
@@ -32,12 +23,7 @@ public class MataKuliahDemo16 {
         arrayOfMataKuliah[0].jumlahJam = 0;
 
         for (int i = 0; i < 3; i++) {
-            System.out.println("Data Mata Kuliah ke-" + (i + 1));
-            System.out.println("Kode        : " + arrayOfMataKuliah[i].kode);
-            System.out.println("Nama        : " + arrayOfMataKuliah[i].nama);
-            System.out.println("SKS         : " + arrayOfMataKuliah[i].sks);
-            System.out.println("Jumlah Jam  : " + arrayOfMataKuliah[i].jumlahJam);
-            System.out.println("---------------------------------");
+            arrayOfMataKuliah[i].cetakInfo(i);
         }
     }
 }
